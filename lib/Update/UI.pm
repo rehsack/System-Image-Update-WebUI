96a0dfdff07783c1e2bc43d8d44fb51b6ce4bd2a
Jens Rehsack <sno@netbsd.org>
2020-10-28 17:13:39 +0100

    Update::UI - provide real {get,set}_sysupdate_config
    Implement and verify reading and saving configuration of the configuration of
    `System::Image::Update` including view in Website frontend.
    
    Signed-off-by: Jens Rehsack <sno@netbsd.org>

--- lib/Update/UI.pm
+++ lib/Update/UI.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 use Dancer2;
-use Update::UI::Backend qw(get_installed_software get_sysupdate_status);
+use Update::UI::Backend qw(get_installed_software get_sysupdate_status get_sysupdate_config set_sysupdate_config);
 
 our $VERSION = '0.001';
 
@@ -17,7 +17,29 @@ get '/' => sub {
 };
 
 get '/config' => sub {
-    template 'config' => {'title' => 'Updater UI - Configuration'};
+    template 'config' => {
+        'title'          => 'Updater UI - Configuration',
+        'sysupdt_config' => get_sysupdate_config
+    };
+};
+
+use Data::Dumper;
+
+post '/apply/config' => sub {
+    my %cfg = (
+        host              => body_parameters->get('host'),
+        path              => body_parameters->get('path'),
+        manifest_basename => body_parameters->get('manifest_basename'),
+        username          => body_parameters->get('username'),
+        password          => body_parameters->get('password'),
+    );
+
+    set_sysupdate_config(\%cfg);
+
+    template 'config' => {
+        'title'          => 'Updater UI - Configuration (Saved)',
+        'sysupdt_config' => get_sysupdate_config
+    };
 };
 
 true;
--- lib/Update/UI/Backend.pm
+++ lib/Update/UI/Backend.pm
@@ -12,7 +12,7 @@ use File::Find::Rule ();
 use File::Slurper qw(read_text);
 use Unix::Statgrab qw(get_process_stats);
 
-our @EXPORT_OK = qw(get_installed_software get_sysupdate_status);
+our @EXPORT_OK = qw(get_installed_software get_sysupdate_status get_sysupdate_config set_sysupdate_config);
 
 sub get_installed_software
 {
@@ -35,7 +35,7 @@ sub get_sysupdate_status
     Update::Status->is_running or return "n/a";
 
     my $us = eval { Update::Status->new(); };
-    $us or return "error: '$@'";
+    $us or return "Update::Status initialization failed: '$@'";
 
     my $status = "idle";
     $us->has_recent_update and $status = "available";
@@ -48,6 +48,40 @@ sub get_sysupdate_status
     grep { /flash-device/ } map { $proc_list->process_name($_) } (0 .. $proc_list->entries() - 1) and $status = "applying";
 
     return $status;
+}
+
+sub get_sysupdate_config
+{
+    my $us = eval { Update::Status->new(); };
+    $us or return {error => "Update::Status initialization failed: '$@'"};
+
+    my %cfg = (
+        host              => $us->update_server,
+        path              => $us->update_path,
+        manifest_basename => $us->update_manifest_basename,
+        ($us->has_http_user   and $us->http_user ne $us->computed_http_cred   ? (username => $us->http_user)   : ()),
+        ($us->has_http_passwd and $us->http_passwd ne $us->computed_http_cred ? (password => $us->http_passwd) : ()),
+    );
+
+    return \%cfg;
+}
+
+sub set_sysupdate_config
+{
+    my $cfg = shift;
+
+    my $us = eval { Update::Status->new(); };
+    $us or return "error: '$@'";
+
+    $cfg->{host} ne $us->update_server                         and $us->update_server($cfg->{host});
+    $cfg->{path} ne $us->update_path                           and $us->update_path($cfg->{path});
+    $cfg->{manifest_basename} ne $us->update_manifest_basename and $us->update_manifest_basename($cfg->{manifest_basename});
+    defined $cfg->{username} and $cfg->{username} ne $us->http_user   and $us->http_user($cfg->{username});
+    defined $cfg->{password} and $cfg->{password} ne $us->http_passwd and $us->http_passwd($cfg->{password});
+
+    $us->save_config_and_restart;
+
+    return;
 }
 
 1;
